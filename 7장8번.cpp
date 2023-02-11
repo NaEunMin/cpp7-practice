e295b73e4ff79b7f8c5af711fff05e661358e5ee
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2023-02-11 12:00:28 +0900

    Add files via upload
    값에 의한 호출로 main문 안에 있는 a는 변하지 않는다.

--- 7장8번.cpp
+++ 7장8번.cpp
@@ -1,9 +1,7 @@
 /******************************************************************************
-
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 
 #include <iostream>
@@ -19,37 +17,20 @@ class Circle{
     void show() {
         cout << "radius = " << radius << " 인 원" << endl;
     }
- /*  
-    Circle& operator ++(){
-        radius++;
-        return *this;
-    }
     
-    Circle operator ++(int x){
-        Circle tmp = *this;
-        radius++;
-        return tmp;
-    }*/
-    friend Circle& operator ++(Circle &c);
-    friend Circle operator ++(Circle &c, int x);
-    
+    friend Circle operator + (int r, Circle c);
+
 };
 
-Circle& operator ++ (Circle &c){
-    c.radius++;
+Circle operator + (int r, Circle c){
+    c.radius += r;
     return c;
 }
 
-Circle operator ++(Circle &c, int x){
-   Circle tmp = c;
-   c.radius++;
-   return tmp;
-}
 int main()
 {
     Circle a(5), b(4);
-    ++a;
-    b = a++;
+    b = 1 + a;
     a.show();
     b.show();
 
